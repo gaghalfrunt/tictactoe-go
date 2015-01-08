7ed4f288a81d3a9dbe8c9366f047b439da98dfaa
Christoph <gaghalfrunt@christophgockel.de>
2015-01-08 13:24:07 +0000

    Adds winner identification to Board

--- src/tictactoe/board.go
+++ src/tictactoe/board.go
@@ -7,6 +7,14 @@ const (
 	O Mark = "o"
 )
 
+func Opponent(mark Mark) Mark {
+	if mark == X {
+		return O
+	} else {
+		return X
+	}
+}
+
 type Board struct {
 	cells [9]Mark
 }
@@ -35,4 +43,111 @@ func (board *Board) Content() map[int]Mark {
 	}
 
 	return content
+}
+
+func (board Board) IsFinished() bool {
+	if board.hasWinner() || board.HasDraw() {
+		return true
+	}
+
+	return false
+}
+
+func (board Board) hasWinner() bool {
+	for _, line := range board.allLines() {
+		if line.hasWinner() {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (board Board) HasDraw() bool {
+	availableMoves := board.availableLocations()
+
+	return len(availableMoves) == 0 && !board.hasWinner()
+}
+
+func (board Board) Winner() Mark {
+	for _, line := range board.allLines() {
+		if line.hasWinner() {
+			return *line.winner()
+		}
+	}
+
+	return ""
+}
+
+func (board Board) allLines() []line {
+	var lines []line
+
+	lines = append(lines, board.rows()...)
+	lines = append(lines, board.columns()...)
+	lines = append(lines, board.diagonals()...)
+
+	return lines
+}
+
+func (board Board) rows() []line {
+	content := board.Content()
+
+	return []line{
+		newLine(content[1], content[2], content[3]),
+		newLine(content[4], content[5], content[6]),
+		newLine(content[7], content[8], content[9]),
+	}
+}
+
+func (board Board) columns() []line {
+	content := board.Content()
+
+	return []line{
+		newLine(content[1], content[4], content[7]),
+		newLine(content[2], content[5], content[8]),
+		newLine(content[3], content[6], content[9]),
+	}
+}
+
+func (board Board) diagonals() []line {
+	content := board.Content()
+
+	return []line{
+		newLine(content[1], content[5], content[9]),
+		newLine(content[3], content[5], content[7]),
+	}
+}
+
+type line struct {
+	elements []Mark
+}
+
+func newLine(marks ...Mark) line {
+	return line{marks}
+}
+
+func (line line) hasWinner() bool {
+	if line.hasUniqueMark(X) || line.hasUniqueMark(O) {
+		return true
+	}
+
+	return false
+}
+
+func (line line) hasUniqueMark(mark Mark) bool {
+	for _, element := range line.elements {
+		if element != mark {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (line line) winner() *Mark {
+	if line.hasWinner() {
+		return &line.elements[0]
+	}
+
+	return nil
 }
--- src/tictactoe/board_test.go
+++ src/tictactoe/board_test.go
@@ -12,29 +12,140 @@ var _ = Describe("Board", func() {
 
 			Expect(board.availableLocations()).To(Equal([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}))
 		})
+
+		It("is not finished", func() {
+			board := Board{}
+
+			Expect(board.IsFinished()).To(BeFalse())
+		})
 	})
 
 	Context("with moves", func() {
 		It("occupies the location", func() {
 			board := Board{}
-			board.Place("x", 1)
+			board.Place(X, 1)
 
 			Expect(board.availableLocations()).To(Equal([]int{2, 3, 4, 5, 6, 7, 8, 9}))
 		})
 
 		It("returns the state of the board", func() {
 			board := Board{}
-			board.Place("x", 1)
-			board.Place("o", 5)
-			board.Place("x", 9)
+			board.Place(X, 1)
+			board.Place(O, 5)
+			board.Place(X, 9)
 
 			expectedContent := map[int]Mark{
-				1: "x", 2: "", 3: "",
-				4: "", 5: "o", 6: "",
-				7: "", 8: "", 9: "x",
+				1: X, 2: "", 3: "",
+				4: "", 5: O, 6: "",
+				7: "", 8: "", 9: X,
 			}
 
 			Expect(board.Content()).To(Equal(expectedContent))
+		})
+	})
+
+	Context("finished", func() {
+		Context("winning", func() {
+			It("row #1", func() {
+				board := Board{}
+				board.Place(X, 1)
+				board.Place(X, 2)
+				board.Place(X, 3)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("row #2", func() {
+				board := Board{}
+				board.Place(X, 4)
+				board.Place(X, 5)
+				board.Place(X, 6)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("row #3", func() {
+				board := Board{}
+				board.Place(X, 7)
+				board.Place(X, 8)
+				board.Place(X, 9)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("column #1", func() {
+				board := Board{}
+				board.Place(X, 1)
+				board.Place(X, 4)
+				board.Place(X, 7)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("column #2", func() {
+				board := Board{}
+				board.Place(X, 2)
+				board.Place(X, 5)
+				board.Place(X, 8)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("column #3", func() {
+				board := Board{}
+				board.Place(X, 3)
+				board.Place(X, 6)
+				board.Place(X, 9)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("diagonal #1", func() {
+				board := Board{}
+				board.Place(X, 1)
+				board.Place(X, 5)
+				board.Place(X, 9)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("diagonal #2", func() {
+				board := Board{}
+				board.Place(X, 3)
+				board.Place(X, 5)
+				board.Place(X, 7)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
+
+			It("knows the winner", func() {
+				board := Board{}
+				board.Place(X, 1)
+				board.Place(X, 2)
+				board.Place(X, 3)
+
+				Expect(board.Winner()).To(Equal(X))
+			})
+		})
+
+		Context("draw", func() {
+			It("is finished", func() {
+				board := Board{}
+
+				board.Place(O, 1)
+				board.Place(O, 2)
+				board.Place(X, 3)
+
+				board.Place(X, 4)
+				board.Place(X, 5)
+				board.Place(O, 6)
+
+				board.Place(O, 7)
+				board.Place(X, 8)
+				board.Place(X, 9)
+
+				Expect(board.IsFinished()).To(BeTrue())
+			})
 		})
 	})
 })
